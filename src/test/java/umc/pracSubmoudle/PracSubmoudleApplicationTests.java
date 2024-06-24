f59809ec3e01fc86eb6c46cc67b00c6eb15862bd
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-06-25 00:05:49 +0900

    Test: submodule test

---
+++ src/test/java/umc/pracSubmoudle/PracSubmoudleApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.pracSubmoudle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracSubmoudleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
